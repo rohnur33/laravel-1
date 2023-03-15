cccc1a97fb94db54b864c728e812b2cf8b181b22
kim <nkim@gmail.com>
2023-03-15 08:17:32 +0700

    menambahkan user request

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,15 +3,17 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Actions\Fortify\PasswordValidationRules;
 
 class UserRequest extends FormRequest
 {
+    use PasswordValidationRules;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,14 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'email' => ['requierd','string','email','max:255','unique:users'],
+            'password' =>$this->passwordRules(),
+            'address' =>['required','string'],
+            'roles' => ['required','string','max:255','in:USER,ADMIN'],
+            'housenumber' => ['required','string','max:255'],
+            'phonenumber' => ['required','string','max:255'],
+            'city' => ['required','string','max:255']
         ];
     }
 }
