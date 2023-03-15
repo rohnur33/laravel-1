413aa2d55401a0dd69bcdcea87aef7eda570dc04
kim <nkim@gmail.com>
2023-03-15 10:48:12 +0700

    menambahkan fungsi foodRequest

--- app/Http/Requests/FoodRequest.php
+++ app/Http/Requests/FoodRequest.php
@@ -11,7 +11,7 @@ class FoodRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,13 @@ class FoodRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'picturePath' => 'required|image',
+            'description' => 'required',
+            'ingredients' => 'required',
+            'price' => 'required|integer',
+            'rate' => 'required|numeric',
+            'types' => ''
         ];
     }
 }
